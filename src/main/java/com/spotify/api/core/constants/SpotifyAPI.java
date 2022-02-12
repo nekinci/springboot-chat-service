2ed19727a6d09f90912aa493873cd4597efb54fb
Niyazi Ekinci <niyaziekinci5050@gmail.com>
2022-02-12 04:20:35 +0300

    Update SpotifyAPI.java

--- src/main/java/com/spotify/api/core/constants/SpotifyAPI.java
+++ src/main/java/com/spotify/api/core/constants/SpotifyAPI.java
@@ -2,8 +2,8 @@ package com.spotify.api.core.constants;
 
 public class SpotifyAPI {
 
-    public static final String CLIENT_ID = "77f63f5e5b224892bc9a246804a4863b";
-    public static final String CLIENT_SECRET = "1fb85abb3af2472983ba3b6a94c21fee";
+    public static final String CLIENT_ID = "CLIENT_ID";
+    public static final String CLIENT_SECRET = "CLIENT_SECRET";
     public static final String REDIRECT_URI = "http://localhost:4200/login";
 
 }
